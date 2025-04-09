6403dbf429fb23da4cd6255c937b9511f2576e03
Ivan Tolkach <ivantolkach2005@gmail.com>
2025-04-09 17:18:21 +0300

    Programme fix

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,15 +2,12 @@ package com.project;
 
 import com.project.ui.ConsoleUI;
 import com.project.config.Config;
-import com.project.util.Logger;
 
 public class Main
 {
     public static void main( String[] args ) {
 
         Config.loadConfig("config.properties");
-
-        Logger.init();
 
         ConsoleUI ui = new ConsoleUI();
         ui.start();
